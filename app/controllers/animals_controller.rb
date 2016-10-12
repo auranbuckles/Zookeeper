60ed98cd89e00e6be2b811fbda3be7bb532f3fa9
auranbuckles <auran59@gmail.com>
2016-10-12 01:36:45 -0400

    added views and controller actions

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,3 +1,33 @@
 class AnimalsController < ApplicationController
 
+	get '/animals' do
+		erb :'/animals/index'
+	end
+
+	# create an animal
+	get '/animals/new' do
+		if logged_in?
+			erb :'/animals/new'
+		else
+			erb :'/signup'
+	end
+
+	post '/animals' do
+		if params[:name] != "" && params[:species] != ""
+			@animal = Animal.new(name: params[:name], species: params[:species], description: params[:description])
+			@user = User.find(session[:user_id])
+			@animal.user_id = @user.id
+			@animal.save
+			redirect_to '/animals/#{@animal.id}'
+		else
+			redirect_to '/animals/new'
+		end
+	end
+
+	# show actions
+
+	get '/animals/:id' do
+
+	end	
+
 end
