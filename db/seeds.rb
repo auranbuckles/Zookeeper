2654070d63a024f47e4fdf59277cdd9c809d1bfa
auranbuckles <auran59@gmail.com>
2016-10-12 11:38:22 -0400

    inserted data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,34 @@
 # ruby encoding: utf-8
 
-User.create(username: 'Heidi', password: 'rainbows', bamboo_shoots: 3, fish: 9, fruits: 9, grass: 12, meat: 7) #5
-User.create(username: 'Sasha', password: 'momomomo', bamboo_shoots: 6, fish: 8, fruits: 8, grass: 10, meat: 8) #6
-User.create(username: 'Abdul', password: 'fire', bamboo_shoots: 12, fish: 6, fruits: 5, grass: 4, meat: 13) #7
-User.create(username: 'Chan', password: 'meowmeow', bamboo_shoots: 8, fish: 8, fruits: 8, grass: 8, meat: 8) #8
-User.create(username: 'Karla', password: 'funfunfun', bamboo_shoots: 5, fish: 14, fruits: 6, grass: 7, meat: 8) #9
+User.create(username: 'Heidi', password: 'rainbows', bamboo_shoots: 3, fish: 9, fruits: 9, grass: 12, meat: 7)
+User.create(username: 'Sasha', password: 'momomomo', bamboo_shoots: 6, fish: 8, fruits: 8, grass: 10, meat: 8)
+User.create(username: 'Abdul', password: 'fire', bamboo_shoots: 12, fish: 6, fruits: 5, grass: 4, meat: 13)
+User.create(username: 'Chan', password: 'meowmeow', bamboo_shoots: 8, fish: 8, fruits: 8, grass: 8, meat: 8)
+User.create(username: 'Karla', password: 'funfunfun', bamboo_shoots: 5, fish: 14, fruits: 6, grass: 7, meat: 8)
+
+Animal.create(name: 'Sky', species: 'dolphin', user_id: 1)
+Animal.create(name: 'Oreo', species: 'penguin', user_id: 2)
+Animal.create(name: 'Mocha', species: 'brown bear', user_id: 3)
+Animal.create(name: 'Snow White', species: 'polar bear', user_id: 4)
+Animal.create(name: 'Snuggles', species: 'seal', user_id: 5)
+
+Animal.create(name: 'Fei Fei', species: 'panda', user_id: 3)
+
+Animal.create(name: 'Lucky', species: 'elephant', user_id: 1)
+Animal.create(name: 'Princess', species: 'monkey', user_id: 2)
+Animal.create(name: 'Slowpoke', species: 'sloth', user_id: 4)
+Animal.create(name: 'Harambe', species: 'ape', user_id: 5)
+
+Animal.create(name: 'Zany', species: 'lion', user_id: 1)
+Animal.create(name: 'Kitty', species: 'tiger', user_id: 2)
+Animal.create(name: 'Ruby', species: 'cheetah', user_id: 3)
+Animal.create(name: 'Bullet', species: 'red fox', user_id: 4)
+Animal.create(name: 'Feist', species: 'wolf', user_id: 5)
+
+Animal.create(name: 'Oliver', species: 'kangaroo', user_id: 1)
+Animal.create(name: 'Stripes', species: 'zebra', user_id: 2)
+Animal.create(name: 'Arella', species: 'buffalo', user_id: 3)
+Animal.create(name: 'Monsoon', species: 'elk', user_id: 4)
+Animal.create(name: 'Beebee', species: 'deer', user_id: 5)
+Animal.create(name: 'Bruno', species: 'rhino', user_id: 1)
 
