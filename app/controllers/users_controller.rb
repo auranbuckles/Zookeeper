cd8cdb280d06824f23141e1822688874544fb9eb
auranbuckles <auran59@gmail.com>
2016-10-12 13:07:42 -0400

    configured routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,34 @@
 class UsersController < ApplicationController
 
+	# show action
+	# redirected after login
+
 	get '/user/:id' do
 		@user = User.find(params[:id])
 		@animals = @user.animals
 		erb :'/users/show'
+	end
+
+	# edit action
+	# purchase more food
+
+	get '/user/:id/edit' do
+		# if logged_in?
+			@user = User.find(params[:id])
+			erb :'/users/edit'
+		# else
+		# 	redirect '/login'
+		# end
+	end
+
+	patch '/user/:id' do
+		@user = User.find(params[:id])
+		if logged_in? && current_user == @user
+			@user.update(bamboo_shoots: @user.bamboo_shoots + 5)
+			redirect "/user/#{@user.id}"
+		else
+			redirect "/user/#{@user.id}/edit"
+		end
 	end
 
 end
